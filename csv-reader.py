7db7e2652362d4db7f7900fe02d86d67125d09b9
Camilo Portilla <31325055+harem123@users.noreply.github.com>
2024-05-17 13:31:47 -0500

    Update csv-reader.py

--- csv-reader.py
+++ csv-reader.py
@@ -2,13 +2,11 @@ import pandas as pd
 csvFile = r"F:\Users\jcportilla\python-csv\activities.csv"
 df_actividades = pd.read_csv(csvFile, delimiter=',')
 
-
-# Example DataFrame
-
-
-
 # Check for 'ANOMALIA' or 'VISITA' in uppercase and tag as 'anomalia', 'visita' or 'normalizacion'
 df_actividades['activities_normalized'] = df_actividades['Actividad'].apply(lambda x: 'anomalia' if ('ANOMALIA' in x or 'LINEA' in x)  else 
                                          ('visita' if 'VISITA' in x else 'normalizacion'))
+df_actividades['datetime'] = pd.to_datetime(df_actividades['Ejecucion'], errors='coerce')
 
-print(df_actividades.head())
+# Extract month and create a new column 'mes'
+df_actividades['mes'] = df_actividades['datetime'].dt.month
+print(df_actividades['mes'])
