4fd9f4f611d8b3b7cf92c55953a3254fbff9ef5f
Camilo Portilla <31325055+harem123@users.noreply.github.com>
2024-05-17 13:07:25 -0500

    Update csv-reader.py

--- csv-reader.py
+++ csv-reader.py
@@ -1,4 +1,14 @@
 import pandas as pd
 csvFile = r"F:\Users\jcportilla\python-csv\activities.csv"
 df_actividades = pd.read_csv(csvFile, delimiter=',')
+
+
+# Example DataFrame
+
+
+
+# Check for 'ANOMALIA' or 'VISITA' in uppercase and tag as 'anomalia', 'visita' or 'normalizacion'
+df_actividades['activities_normalized'] = df_actividades['Actividad'].apply(lambda x: 'anomalia' if ('ANOMALIA' in x or 'LINEA' in x)  else 
+                                         ('visita' if 'VISITA' in x else 'normalizacion'))
+
 print(df_actividades.head())
